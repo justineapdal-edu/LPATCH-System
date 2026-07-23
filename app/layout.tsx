6c5749a9aeb19c5f61f571b8ad3cde13bead40ca
Justine Apdal <justineapdal.edu@gmail.com>
2026-07-24 00:28:51 +0800

    fix sidebar min-full heigt, change to full height

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="h-full flex flex-col">
         <AppShell>{children}</AppShell>
       </body>
     </html>
